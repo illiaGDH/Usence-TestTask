436183dff4cad422a6112cef00ee9ecd59e4edf8
GDHohil <korec.illia@gmail.com>
2021-07-09 02:47:37 +0300

    created basic setup to work with

---
+++ src/app/string-generator/string-generator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-string-generator',
+  templateUrl: './string-generator.component.html',
+  styleUrls: ['./string-generator.component.scss']
+})
+export class StringGeneratorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
